9915d7c1c33366d82a9f1920824affa43dd03475
Jorgelenny47 <101591320+Jorgelenny47@users.noreply.github.com>
2025-09-18 09:11:19 +0200

    Feat: "adding" genre

--- src/AppForSEII2526.API/Models/Genre.cs
+++ src/AppForSEII2526.API/Models/Genre.cs
@@ -2,5 +2,8 @@
 {
     public class Genre
     {
+        // It is time
+
+        // I was meant to just add genre to models but I reverted the revert for fun and thus it was already there so I guess I'll write this nonsense comment
     }
 }
